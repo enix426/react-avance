04ea152055772c7b85cf66e13daaee62fb006415
Abbes Boulebtina <imrane.business01@gmail.com>
2024-11-17 10:42:54 -0500

    Examples.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import Bienvenue, { Compteur, Welcome } from "./components/ch02/Examples";
-import SignupForm from "./components/ch06/SignupForm";
-import SimpleForm from "./components/ch06/SimpleForm";
-import StyledSimpleForm from "./components/ch06/StyledSimpleForm";
-import Comments from "./components/ch07/comments/Comments";
-import PostList from "./components/ch07/PostList";
 
 function App() {
   return (
@@ -11,13 +6,6 @@ function App() {
       <Bienvenue nom="John" />
       <Welcome nom="Alex" />
       <Compteur />
-
-      <SimpleForm />
-      <StyledSimpleForm />
-      <SignupForm />
-
-      <PostList />
-      <Comments />
     </>
   );
 }
