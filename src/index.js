cfdbe3dae9b6fe759f39ce31fb2b4a9035df335f
Abbes Boulebtina <imrane.business01@gmail.com>
2024-11-26 17:52:30 -0500

    small change.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/ch04/Exercise/App';
+import App from './components/ch04/App01';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
