68c326f55c5a4118e8af961262c71aeccb05dfbc
Abbes Boulebtina <imrane.business01@gmail.com>
2024-11-30 16:21:48 -0500

    Exemple.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/ch05/App';
+import App from './components/ch05/exercise/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
